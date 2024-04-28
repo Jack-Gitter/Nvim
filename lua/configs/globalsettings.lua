2bb14bd02ae6a9a09d6d9a67de4a31d15d1c7591
Jack Gitter <jackgitter@Jacks-MacBook-Pro.local>
2024-04-28 17:32:10 -0400

    nice

--- lua/configs/globalsettings.lua
+++ lua/configs/globalsettings.lua
@@ -1,15 +1,19 @@
--- basic settings
+-- vim options
 vim.opt.termguicolors = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4 
 vim.opt.expandtab = true
 vim.opt.laststatus = 3
-vim.cmd.colorscheme('embark')
 vim.opt.statuscolumn = "%{vim.b.gitsigns_status}%=%r|"
 
+-- vim cmds
+vim.cmd.colorscheme('embark')
+
+-- vim keybindings
 vim.keymap.set('i', 'jk', '<Esc>')
 
+-- highlight group settings
 vim.api.nvim_set_hl(0, 'GitSignsAdd', { fg = "#66ff00", bg = "" })
 vim.api.nvim_set_hl(0, 'GitSignsChange', { fg = "#FFFF00", bg = "" })
 vim.api.nvim_set_hl(0, 'GitSignsDelete', { fg = "#FF1100", bg = "" })
