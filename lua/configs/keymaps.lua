9323cd482937ad62e0d1baf0734cc19c1922d8ca
Jack Gitter <jackgitter@Jacks-MacBook-Pro.local>
2024-05-04 16:45:51 -0400

    eh

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -5,10 +5,10 @@ vim.g.mapleader = " "
 
 -- telescope keybinings
 local t_builtin = require('telescope.builtin')
-vim.keymap.set("n", "<leader>ff", function() require("telescope").extensions.file_browser.file_browser() end)
-vim.keymap.set('n', '<leader>fg', t_builtin.live_grep, {})
-vim.keymap.set('n', '<leader>bb', t_builtin.buffers, {})
-vim.keymap.set('n', '<leader>gd', t_builtin.lsp_definitions, {})
-vim.keymap.set('n', '<leader>gi', t_builtin.lsp_implementations, {})
+vim.keymap.set("n", "<leader>f", function() require("telescope").extensions.file_browser.file_browser() end)
+vim.keymap.set('n', '<leader>g', t_builtin.live_grep, {})
+vim.keymap.set('n', '<leader>b', t_builtin.buffers, {})
+vim.keymap.set('n', '<leader>d', t_builtin.lsp_definitions, {})
+vim.keymap.set('n', '<leader>i', t_builtin.lsp_implementations, {})
 vim.keymap.set('n', '<leader>v', t_builtin.git_branches, {})
-vim.keymap.set('n', '<leader>dd', function () t_builtin.diagnostics({bufnr=0}) end, {})
+vim.keymap.set('n', '<leader>e', function () t_builtin.diagnostics({bufnr=0}) end, {})
