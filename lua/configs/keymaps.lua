5c65a1e692751028447a54b6c56a2a9dca44ebf0
Jack Gitter <jackgitter@Jacks-MacBook-Pro.local>
2024-05-05 20:33:37 -0400

    nice

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -5,11 +5,12 @@ vim.g.mapleader = " "
 -- telescope keybinings
 local t_builtin = require('telescope.builtin')
 
-vim.keymap.set("n", "<leader>f", function() require("telescope").extensions.file_browser.file_browser() end)
+vim.keymap.set("n", "<leader>fe", function() require("telescope").extensions.file_browser.file_browser() end)
+vim.keymap.set("n", "<leader>ff", t_builtin.find_files, {})
 vim.keymap.set('n', '<leader>g', t_builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', t_builtin.buffers, {})
 vim.keymap.set('n', '<leader>d', t_builtin.lsp_definitions, {})
-vim.keymap.set('n', '<leader>i', t_builtin.lsp_implementations, {})
+vim.keymap.set('n', '<leader>r', t_builtin.lsp_references, {})
 vim.keymap.set('n', '<leader>v', t_builtin.git_branches, {})
 vim.keymap.set('n', '<leader>e', function () t_builtin.diagnostics({bufnr=0}) end, {})
 
