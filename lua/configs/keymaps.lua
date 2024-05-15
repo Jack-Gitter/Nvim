ab4956b5ceb99e2329ba094b52bde086b3d01acd
Jack Gitter <jack.gitter@gmail.com>
2024-05-14 22:12:40 -0400

    all lsp stuff configured

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -8,13 +8,14 @@ local t_builtin = require('telescope.builtin')
 
 vim.keymap.set("n", "<leader>fe", function() require("telescope").extensions.file_browser.file_browser() end)
 vim.keymap.set("n", "<leader>ff", t_builtin.find_files, {})
-vim.keymap.set('n', '<leader>g', t_builtin.live_grep, {})
+vim.keymap.set('n', '<leader>gr', t_builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', function () t_builtin.buffers({sort_mru=true, ignore_current_buffer=true}) end, {})
 vim.keymap.set('n', '<leader>d', t_builtin.lsp_definitions, {})
 vim.keymap.set('n', '<leader>r', t_builtin.lsp_references, {})
 vim.keymap.set('n', '<leader>v', t_builtin.git_branches, {})
 vim.keymap.set('n', '<leader>e', function () t_builtin.diagnostics({bufnr=0}) end, {})
 vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, {})
+vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, {})
 vim.keymap.set('n', ';', '%', {})
 
 -- leap bindings
@@ -23,7 +24,7 @@ vim.keymap.set('n', 'F', '<Plug>(leap-backward)')
 
 -- gitsigns
 local gitsigns = require('gitsigns')
-vim.keymap.set('n', '<leader>h', gitsigns.preview_hunk, {})
+vim.keymap.set('n', '<leader>gh', gitsigns.preview_hunk, {})
 
 -- diagnostics
 vim.keymap.set("n", "<leader>sd", function() vim.diagnostic.open_float() end, {})
