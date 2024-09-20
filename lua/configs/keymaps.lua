ba47222a99c3c73fca68c2b851b2bb3433f5518d
Jack Gitter <jack.gitter@sentinelgroup.com>
2024-09-20 14:56:11 -0400

    beauty

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -24,6 +24,8 @@ vim.keymap.set('n', '<leader>md', function() vim.cmd("Glow") end, {})
 -- leap bindings
 vim.keymap.set('n', 'f', '<Plug>(leap-forward)')
 vim.keymap.set('n', 'F', '<Plug>(leap-backward)')
+vim.keymap.set('v', 'f', '<Plug>(leap-forward)')
+vim.keymap.set('v', 'F', '<Plug>(leap-backward)')
 
 -- gitsigns
 local gitsigns = require('gitsigns')
