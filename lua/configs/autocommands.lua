5dca9d82954a0cd99bcb99dfc9fbcce5cf06e094
Jack Gitter <jack.gitter@sentinelgroup.com>
2024-11-20 12:10:18 -0500

    plugin for prettier

--- lua/configs/autocommands.lua
+++ lua/configs/autocommands.lua
@@ -5,3 +5,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
         vim.highlight.on_yank({higroup='Cursor'})
     end
 })
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+    desc = 'highlight yanked texted',
+    pattern = {'javascript', 'typescript'},
+    callback = function ()
+        vim.cmd(":Prettier")
+    end
+})
